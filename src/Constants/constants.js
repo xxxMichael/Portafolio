a2356e3397cbd6961434a773a8bf04185fde146a
xxxMichael <mayckolchavez27@gmail.com>
2024-11-23 17:07:28 -0500

    se agrega el componente nav y complementos como las constantes  y estilos js

---
+++ src/Constants/constants.js
@@ -0,0 +1,66 @@
+/*
+   Copyright (C), 2023-2024, Sara Echeverria (bl33h)
+   Author: Sara Echeverria
+   FileName: constants.js
+   Version: I
+   Creation: 02/06/2023
+   Last modification: 18/12/2023
+*/
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faX,
+  faBars,
+  faWindowRestore,
+  faBagShopping,
+  faDiceD6,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faReact,
+  faGithub,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
+
+import {
+  htmlIcon,
+  cssIcon,
+  jsIcon,
+  reactIcon,
+  awsIcon,
+  javaIcon,
+  gitIcon,
+  githubIcon,
+  psqlIcon,
+  eslintIcon,
+  pyIcon,
+  viteIcon,
+  nodeIcon,
+  raspIcon,
+  neoIcon,
+  figmaIcon,
+  pawsitivePrototype,
+  memoryGame,
+  avatar,
+} from '../assets';
+
+library.add(faX, faBars, faWindowRestore, faBagShopping, faDiceD6);
+
+export const navLinks = [
+    {
+      id: 'about',
+      title: 'About',
+    },
+    {
+      id: 'projects',
+      title: 'Projects',
+    },
+    {
+      id: 'skills',
+      title: 'Skills',
+    },
+    {
+      id: 'contact',
+      title: 'Contact',
+    },
+  ];
