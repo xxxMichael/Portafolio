263b90c8647640e05f33174c275f1dce8614a759
xxxMichael <mayckolchavez27@gmail.com>
2024-11-24 16:14:01 -0500

    Se agregan las paginas a app

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,19 @@
-import NavBar from "../Components/nav/NavBar.jsx";
-
+import NavBar from "../Components/nav/NavBar";
+import Hero from "../Pages/Hero";
+import Projects from "../Pages/Projects";
+import About from "../Pages/About";
+import Skills from "../Pages/Skills";
+import Contact from "../Pages/Contact";
 
 function App() {
   return (
     <>
       <NavBar />
-    
+      <Hero />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
     </>
   );
 }
