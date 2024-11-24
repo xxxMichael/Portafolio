99962e119f8bb7dcd1357b45dd0c3d65be6d126c
xxxMichael <mayckolchavez27@gmail.com>
2024-11-24 00:45:10 -0500

    se agrega la ruta de git pages para poder desplegar el proyecto

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vite.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
   base:"https://xxxmichael.github.io/Portafolio"
-})
+  
+});
