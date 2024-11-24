e722d9f9f35410217a8bd7eb4091169170815990
xxxMichael <mayckolchavez27@gmail.com>
2024-11-24 17:37:17 -0500

    Se actualizan SocialLinks de github linkedin y email

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -6,19 +6,19 @@ import { icons } from "../Constants/constants";
 const SocialLinks = () => {
   return (
     <div className="w-full h-[100px] bg-grayscale-950 flex justify-center items-center gap-3 p-6">
-      <a href="" target="_blank">
+      <a href="https://github.com/xxxMichael" target="_blank">
         <FontAwesomeIcon
           className="text-3xl text-grayscale-25 hover:text-primary-200 hover:scale-[1.1] transition-all duration-75"
           icon={icons.faGithub}
         />
       </a>
-      <a href="" target="_blank">
+      <a href="mailto:mayckolchavez27@gmail.com" target="_blank">
         <FontAwesomeIcon
           className="text-3xl text-grayscale-25 hover:text-primary-200 hover:scale-[1.1] transition-all duration-75"
           icon={icons.faEnvelope}
         />
       </a>
-      <a href="" target="_blank">
+      <a href="www.linkedin.com/in/michael-chavez-b43924227" target="_blank">
         <FontAwesomeIcon
           className="text-3xl text-grayscale-25 hover:text-primary-200 hover:scale-[1.1] transition-all duration-75"
           icon={icons.faLinkedin}
